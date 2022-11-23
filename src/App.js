556308c997d0e51752fc2c7b48bfbd00129c6f09
Rahul <kattirahul996@gmail.com>
2022-11-23 19:01:11 +0530

    message

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@
 // import RemoveDuplicateCharacter from './RemoveDuplicateCharacter';
 // import Style from './Style';
 
+import TodoMethod from "./TodoMethod";
+
+// import { useState } from "react";
+// import Information from "./Information";
+
+
+
 function App() {
+  // const name = 'Snehal' ;
+
+  // const [data,setData] = useState("")
   return (
     <div className="App">
       {/* <RemoveDuplicateCharacter /> */}
       {/* <Style /> */}
     {/* <Cmd /> */}
+    {/* <h1> parent Component :  {data}</h1> */}
+    {/* <Information setData = {setData}  info = {name}/> */}
+
+    <TodoMethod />
     </div>
   );
 }
--- src/Information.js
+++ src/Information.js
@@ -1,6 +1,25 @@
-const Information = () => {
+import React from "react"
+
+
+const Information = ( {setData}) => {
+
+  // OR
+
+  // const Information = ( props,  {setData}) => {
+
+  
+
+  const myData = "abc";
+
     return (
-      <div>Information</div>
+      <div>
+        <h1>Child Component :</h1>
+        <button type="button" onClick={()=> setData(myData)}>Click me </button> <br />
+         <label>firstName</label>
+        {/* <input type = "text" value={props.info}></input> */}
+
+       
+      </div>
     )
   }
   
