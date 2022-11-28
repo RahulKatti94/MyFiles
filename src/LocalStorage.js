7e7ad29e516b09005bf5a286be435938c8a52108
Rahul <kattirahul996@gmail.com>
2022-11-28 17:17:13 +0530

    message

---
+++ src/LocalStorage.js
@@ -0,0 +1,19 @@
+ import { useState , useEffect } from "react";
+
+
+ const localStorage = () =>{
+
+    const [data, setData] = useState();
+
+    useEffect = ()=>{
+
+    }
+
+    return (
+        <div>
+
+        </div>
+    )
+ }
+
+ export default localStorage
